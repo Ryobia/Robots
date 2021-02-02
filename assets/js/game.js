3cb955c25b532e2a8abce6fb63d5d5ebdca2a094
Ryobia <ryobia36@gmail.com>
2021-02-01 20:14:47 -0700

    added like 10 lines to js, nothing intersting yet

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//game begins
+//fight();
+
+//sets playerName to the name of users robot
+var playerName = window.prompt("What is your robot's name");
+
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
