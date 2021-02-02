59755a2019241d826b4a103dd4a0ed8fe16fa191
Ryobia <ryobia36@gmail.com>
2021-02-02 12:49:32 -0700

    it makes more sense and like, works better ya know?

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,7 +64,17 @@ var fight = function() {
 }
 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to The Jungle " + playerName + "! Round " + ( i + 1) );
+
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+
+     else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+      }
+    
 }
