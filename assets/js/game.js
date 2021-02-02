eb7b89d3ac30364fbd06c366236830d591cef415
Ryobia <ryobia36@gmail.com>
2021-02-01 20:44:06 -0700

    It kinda does stuff now but not really

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,40 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-//game begins
-//fight();
+console.log(playerName, playerHealth, playerAttack);
 
-//sets playerName to the name of users robot
-var playerName = window.prompt("What is your robot's name");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("Our robot's name is " + playerName);
+var fight = function() {
+    window.alert("WELCOME TO THE JUNGLE " + playerName + "!");
 
-var name = "your name";
-console.log(name);
+    enemyHealth = enemyHealth - playerAttack;
+    console.log("Roborto's health is now " + enemyHealth);
+    enemyHealth - playerAttack;
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(playerName + " health is now " + playerHealth);
+    playerHealth - enemyAttack;
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " is out of commission");
+    }
+    else {
+        window.alert(enemyName + " has " + enemyHealth + " health remaining.");
+    }
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " is straight up, not having a good time... he died.");
+    }
+    else {
+        window.alert(playerName + " has " + playerHealth + " health remaining.");
+    }
+
+
+};
+
+fight();
 
